bb5cc8c355aaae264c39db2cf0264fe30c3d55e3
Daniel Hjort <daniel.hjort@patchworksolutions.com>
2010-08-22 00:03:57 +0200

    Parse and add links in Twitter feed

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ var feed = "";
 function twitterfeed(twitterfeed) {
 	feed = "<h1>News</h1>";
 	for (i=0; i<twitterfeed.length; i++) {
-		feed = feed.concat("<h2>", twitterfeed[i].created_at.substr(0, 16),"</h2><p>",twitterfeed[i].text,"</p>");
+		feed = feed.concat("<h2>", twitterfeed[i].created_at.substr(0, 16),"</h2><p>",twitterfeed[i].text.parseURL().parseHashtag().parseUsername(),"</p>");
 	}
 }
 
@@ -61,3 +61,28 @@ function showContactInfo() {
     document.getElementById("home").style.visibility = "hidden";
     document.getElementById("aboutus").style.visibility = "hidden";
 }
+
+/*
+* Twitter parse methods from
+* http://www.simonwhatley.co.uk/examples/twitter/prototype/
+* Modified to replace all occurences. Added /g.
+*/
+String.prototype.parseURL = function() {
+	return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
+		return url.link(url);
+	});
+};
+
+String.prototype.parseUsername = function() {
+	return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+		var username = u.replace("@","")
+		return u.link("http://twitter.com/"+username);
+	});
+};
+
+String.prototype.parseHashtag = function() {
+	return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+		var tag = t.replace("#","%23")
+		return t.link("http://search.twitter.com/search?q="+tag);
+	});
+};
